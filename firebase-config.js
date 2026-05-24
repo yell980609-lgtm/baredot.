aa4aa4ff48b188e0b1921774bde7507bf6a213f4
yell980609-lgtm <yell980609@gmail.com>
2026-05-24 21:28:32 +0900

    Add Firebase config placeholder

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+  messagingSenderId: 'YOUR_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+export const adminEmails = [
+  'YOUR_EMAIL@example.com',
+];
